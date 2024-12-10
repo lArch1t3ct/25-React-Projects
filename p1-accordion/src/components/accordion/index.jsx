1b2b11e17d99315e358cf1d44e92c1b142179ec4
lArch1t3ct <167084066+lArch1t3ct@users.noreply.github.com>
2024-12-10 19:09:32 +0200

    Multi Select Functionality
    Two more states were added to keep track:
    --> Whether the multi select is enabled
    --> What accordion elements has been selected
    
    The "onClick" attribute of the <div class="title"> was updated so that:
    --> When the multi select is enabled, the click event will be handled by the
        handleMultiSelection event handler
    --> Otherwise, by the handleSingleSelection
    
    Finally, the conditional rendering was updated so that when the id of the clicked title
    is not in the multiAccordion array, its answer property to be displayed, that is for the
    accordion element to be expanded.

--- p1-accordion/src/components/accordion/index.jsx
+++ p1-accordion/src/components/accordion/index.jsx
@@ -4,28 +4,49 @@ import './styles.css';
 
 function Accordion(){
     const [selected, setSelected] = useState(undefined);
+    const [isMultiselectOn, setIsMultiselectOn] = useState(false);
+    const [multipleAccordion, setMultipleAccordion] = useState([]);
 
     const handleSingleSelection = (getQuestionId) => {
         setSelected(getQuestionId === selected ? null : getQuestionId);
 
     }
+
+    const handleMultiSelection = (getQuestionId) => {
+        let newState = [...multipleAccordion];
+        const currentIdIndex = newState.indexOf(getQuestionId);
+
+        if(currentIdIndex === -1){
+            newState.push(getQuestionId);
+        } else {
+            newState.splice(currentIdIndex, 1);
+        }
+
+        setMultipleAccordion(newState);
+    }
+
+    console.log(selected, multipleAccordion);
     return (
         <div className="wrapper">
-            <button>Enable Multi Selection</button>
+            <button onClick={ () => setIsMultiselectOn(!isMultiselectOn) }>Enable Multi Selection</button>
             <div className="accordion">
                 {
                     data && data.length > 0
                     ? data.map( (dataItem) => (
                         <div className="item">
                             <div className="title"
-                                 onClick={ () => handleSingleSelection(dataItem.id) }
+                                 onClick={ 
+                                    isMultiselectOn
+                                    ? () => handleMultiSelection(dataItem.id)
+                                    : () => handleSingleSelection(dataItem.id)
+                                  }
                             >
                                 <h3>{ dataItem.question }</h3>
                                 <span>+</span>
                             </div>
                             <div className="answer">
                                 {
-                                    selected === dataItem.id
+                                    selected === dataItem.id || multipleAccordion.indexOf(dataItem.id) !== -1
                                     ? <div className="content">{ dataItem.answer }</div>
                                     : null
                                 }
