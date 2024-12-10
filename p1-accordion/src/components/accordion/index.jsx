81bb8e1a6f84c7a5e442a604309af3db58ffc8fb
lArch1t3ct <167084066+lArch1t3ct@users.noreply.github.com>
2024-12-10 16:33:01 +0200

    Collapsable Functionality
    The collaspable functionality was added.
    When the user clicks on the title or the "+" icon, the description is displayed.
    When clicks again on the same title, the description vanishes.
    At any given time, only one Accordion element is expanded.

--- p1-accordion/src/components/accordion/index.jsx
+++ p1-accordion/src/components/accordion/index.jsx
@@ -4,6 +4,10 @@ import data from "./data";
 function Accordion(){
     const [selected, setSelected] = useState(undefined);
 
+    const handleSingleSelection = (getQuestionId) => {
+        setSelected(getQuestionId === selected ? null : getQuestionId);
+
+    }
     return (
         <div className="wrapper">
             <div className="accordion">
@@ -11,11 +15,17 @@ function Accordion(){
                     data && data.length > 0
                     ? data.map( (dataItem) => (
                         <div className="item">
-                            <div className="title">
-                                <h3>{dataItem.question}</h3>
+                            <div className="title"
+                                 onClick={ () => handleSingleSelection(dataItem.id) }
+                            >
+                                <h3>{ dataItem.question }</h3>
                                 <span>+</span>
                             </div>
-                            
+                            {
+                                selected === dataItem.id
+                                ? <div className="content">{ dataItem.answer }</div>
+                                : null
+                            }
                         </div>
                     ) )
                     : <div>No data was returned!</div>
